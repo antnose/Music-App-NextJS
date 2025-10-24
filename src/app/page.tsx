c4659dc6405f2267accb205cb1e7fdfaff48d784
antnose <antnose28@gmail.com>
2025-10-24 19:32:02 +0600

    Basic setup of nextjs application and make a navbar using aceternity ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-4xl font-bold">Chai Aur Code</h1>
+    </main>
+  );
+};
+
+export default Home;
